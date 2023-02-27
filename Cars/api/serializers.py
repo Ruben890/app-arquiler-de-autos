e30bc9753d3ec23205c17d130a78610256f2448e
Darlin Ruben Nina C <chinausto@gmail.com>
2023-02-26 23:20:56 -0400

    models_cars04

--- Cars/api/serializers.py
+++ Cars/api/serializers.py
@@ -2,10 +2,7 @@ from rest_framework import serializers
 from ..models import cars, year_cars, brand_cars, guys_cars
 
 
-class CarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = cars
-        fields = "__all__"
+
 
 class YearCarSerializer(serializers.ModelSerializer):
      class Meta:
@@ -20,4 +17,12 @@ class BrandCarSerializer(serializers.ModelSerializer):
 class GuyCarSerializer(serializers.ModelSerializer):
      class Meta:
         model = guys_cars
+        fields = "__all__"
+
+class CarSerializer(serializers.ModelSerializer):
+    year = YearCarSerializer()
+    guys = GuyCarSerializer()
+    brand = BrandCarSerializer()
+    class Meta:
+        model = cars
         fields = "__all__"
