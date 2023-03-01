4f1087a4d539217c4a37fffaa7b06fd025e37dab
Darlin Ruben Nina C <chinausto@gmail.com>
2023-03-01 16:20:38 -0400

    users_login003

--- users/api/api.py
+++ users/api/api.py
@@ -3,10 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed
 
-import jwt, datetime
+import  jwt
+import datetime
 from .serializers import ProfilesSerializer, Login
 from users.models import Profiles
-  
+
 
 class Register(viewsets.GenericViewSet):
     model = Profiles
@@ -37,16 +38,19 @@ class Login(viewsets.GenericViewSet):
         # ?confirm if the password is correct
         if not user.check_password(password):
             raise AuthenticationFailed("Users password is incorrect")
-        
-        ###? Useful load
-        payload  = {
-            "id":  user.id, ###* User Identifier ID
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),###* Useful life of Token
-            "lat": datetime.datetime.utcnow() ###* time in which the token was created
-        }
 
+        # ? Useful load
+        payload = {
+            "id":  user.id,  # * User Identifier ID
+            # * Useful life of Token
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            "iat": datetime.datetime.utcnow()  # * time in which the token was created
+        }
+        # ?generic token
+        token = jwt.encode(payload=payload, key='secret',algorithm="HS256")
         return Response({
-            "http": 200, 
+            "http": 200,
             "message": "login Successful",
-            
+            "jwt": token
+
         })
