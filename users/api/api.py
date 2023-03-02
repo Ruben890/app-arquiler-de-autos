e959f2a1ef65754c795be3f4581d780d8f872f75
Darlin Ruben Nina C <chinausto@gmail.com>
2023-03-02 00:12:12 -0400

    user_login005

--- users/api/api.py
+++ users/api/api.py
@@ -2,9 +2,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed
-
 import jwt
-import datetime
+from .authentication import create_auth_token 
 from .serializers import ProfilesSerializer, Login
 from users.models import Profiles
 
@@ -38,37 +37,27 @@ class Login(viewsets.GenericViewSet):
         # ?confirm if the password is correct
         if not user.check_password(password):
             raise AuthenticationFailed("Users password is incorrect")
-
-        # ? Useful load
-        payload = {
-            "id":  user.id,  # * User Identifier ID
-            # * Useful life of Token
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            "iat": datetime.datetime.utcnow()  # * time in which the token was created
-        }
         # ?generic token
-        token = jwt.encode(payload=payload, key='secret', algorithm="HS256")
+        access_token = create_auth_token(user.id)
         # ? configuration cookie
         response = Response()
-        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.set_cookie(key='create_auth_token', value=access_token, httponly=True)
 
         response.data = {
             "http": 200,
-            "message": "login Successful",
-            "jwt": token
+            "message": "login Successful"
         }
         return response
 
 
 class UsersViw(viewsets.GenericViewSet):
-
     def list(self, request):
         # ? verifying cookie token
-        token = request.COOKIES.get('jwt')
+        token = request.COOKIES.get('create_auth_token')
         if not token:
             raise AuthenticationFailed('Unauthenticated!')
         try:
-            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+            payload = jwt.decode(token, 'access_secret', algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
             raise AuthenticationFailed('Unauthenticated!')
         # ? filtering the user who owns the token using the id
@@ -76,13 +65,14 @@ class UsersViw(viewsets.GenericViewSet):
         serializer = ProfilesSerializer(user)
         # ? response to Users who own the token
         return Response(serializer.data)
-
+    
+    
 
 class Logout(viewsets.GenericViewSet):
     # ? delete the cookie
     def create(self, request):
         response = Response()
-        response.delete_cookie('jwt')
+        response.delete_cookie('create_auth_token')
         response.data = {
             "message": "logout Successfully"
         }
---
+++ users/api/authentication.py
@@ -0,0 +1,16 @@
+from rest_framework.exceptions import AuthenticationFailed
+import jwt
+import datetime
+from ..models import Profiles
+
+def create_auth_token(id):
+    return jwt.encode(payload={
+         # ?User Identifier ID
+            "id":  id,
+            # ? Useful life of Token
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=30),
+            # ? time in which the token was created
+            "iat": datetime.datetime.utcnow()},
+                      key='access_secret', algorithm='HS256')
+
+
