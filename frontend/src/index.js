96f88f733dbf0f05ad6a44041aedd0035a82ca43
Darlin Ruben Nina C <chinausto@gmail.com>
2023-03-21 19:16:22 -0400

    dependecias

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Routers from "./Routers/routers";
-import { store } from "./apps/store";
-import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}><Routers /></Provider>
+  <Routers />
 );
