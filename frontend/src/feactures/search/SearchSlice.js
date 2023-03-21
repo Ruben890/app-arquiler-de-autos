3706d3a46dc067b12ac750683919ad626dee48c2
Darlin Ruben Nina C <chinausto@gmail.com>
2023-03-21 15:57:11 -0400

    services00

---
+++ frontend/src/feactures/search/SearchSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const Search = createSlice({
+    name: 'Search',
+    initialState: [],
+    reducers: {}
+})
+
+
+export default Search
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap'
+import "bootstrap-icons/font/bootstrap-icons.css";
 import Routers from "./Routers/routers";
-
-
+import { store } from "./apps/store";
+import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Routers />);
+root.render(
+    <Provider store={store}><Routers /></Provider>
+);
