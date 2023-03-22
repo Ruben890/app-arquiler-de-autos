b54697c4ae0c324b0d2571716e0e75e34529d898
Darlin Ruben Nina C <chinausto@gmail.com>
2023-03-22 19:33:40 -0400

    config_react_query001

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Routers from "./Routers/routers";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Routers />
+  <QueryClientProvider client={queryClient}>
+    <Routers />
+    <ReactQueryDevtools />
+  </QueryClientProvider>
+
 );
